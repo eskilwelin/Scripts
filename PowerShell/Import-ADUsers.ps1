56ed7417231cc572f594d76af8c34e1c564951e7
Eskil Welin <eskilwelin@gmail.com>
2026-07-09 22:16:53 +0200

    Clean Code

--- PowerShell/Import-ADUsers.ps1
+++ PowerShell/Import-ADUsers.ps1
@@ -38,25 +38,22 @@ foreach ($User in $ADUsers) {
 		# Validate Sam 
 		$SamAccountName = $GivenName.Substring(0,1).ToLower() + '.' + $Surname.ToLower()
 		$SamAccountName = Get-ValidSam -Sam $SamAccountName
-
-
-		$Email = Get-UPN -Sam $SamAccountName
-		$UPN = Get-UPN -Sam $SamAccountName
 		
 		$UserInfo = @{
 			Name = $User.name
 			GivenName = $GivenName
 			Surname = $Surname
 			SamAccountName = $SamAccountName
-			UserPrincipalName = $UPN
+			UserPrincipalName = Get-UPN -Sam $SamAccountName
 			ChangePasswordAtNextLogon = $true
 			Enabled = $true
-			EmailAddress = $Email
+			EmailAddress = Get-UPN -Sam $SamAccountName
 			Title = $User.title
 			Department = $User.department
+			# Change this - Hardcoded creds 
 			AccountPassword = (ConvertTo-SecureString "P@ssw0rd123!" -AsPlainText -Force)
 		}
-		# Change this to check for UID
+		# Change this to validate against UID
 		if (Get-ADUser -Filter "SamAccountName -eq '$SamAccountName'") {
 			Write-Host "A user with username $SamAccountName already exists in $Domain" -ForegroundColor Yellow
 			continue
